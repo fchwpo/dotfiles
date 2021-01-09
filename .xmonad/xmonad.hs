f6000286bdc1e99d9e3404100cc47ac61b5482c4
Shubham Vishwakarma <shubham.vishwakarma@justdial.com>
2021-01-09 23:08:15 +0530

    Minor changes in tint2

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -208,7 +208,7 @@ mySpacing' i = spacingRaw True (Border i i i i) True (Border i i i i) True
 tall     = renamed [Replace "tall"]
            $ windowNavigation
            $ addTabs shrinkText myTabTheme
-           -- $ subLayout [] (smartBorders Simplest)
+           $ subLayout [] (smartBorders Simplest)
            $ limitWindows 12
            $ mySpacing 8
            $ ResizableTall 1 (3/100) (1/2) []
